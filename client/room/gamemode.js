c5eab28afca66486f7db4ad50091a709f98a508b
Andrew <139539083+Andrew001-png@users.noreply.github.com>
2025-11-03 23:51:01 +0900

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -6,10 +6,11 @@ import { Color } from 'pixel_combats/basic';
 var WaitingPlayersTime = 10;
 var BuildBaseTime = 30;
 var GameModeTime = 1800;
-var DefPoints = 15;
+var DefPoints = 20;
 var EndOfMatchTime = 10;
 var DefPointsMaxCount = 30;
 var DefTimerTickInderval = 1;
+var SavePointsCount = 10;
 var RepairPointsBySecond = 0.5;
 var CapturePoints = 10;                // сколько очков нужно для захвата
 var MaxCapturePoints = 15;        // сколько макс очков
@@ -228,7 +229,7 @@ function DefTriggerUpdate() {
         // если красных нет в зоне то восстанавливаются очки
         if (redCount == 0) {
                 // восстанавливаем очки до несгораемой суммы
-                if (blueTeam.Properties.Get("Deaths").Value != 0)
+                if (blueTeam.Properties.Get("Deaths").Value % SavePointsCount != 0)
                         blueTeam.Properties.Get("Deaths").Value += RepairPointsBySecond;
                 // синим идет подска об обороне зоны
                 if (stateProp.Value == GameStateValue)
